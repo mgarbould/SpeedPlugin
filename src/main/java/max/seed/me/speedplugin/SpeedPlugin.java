4582494b15f7b004a9c2b676249016720c874b19
codebeast1997 <maxgarbould@gmail.com>
2023-01-22 00:21:13 +0000

    Initial commit

---
+++ src/main/java/max/seed/me/speedplugin/SpeedPlugin.java
@@ -0,0 +1,21 @@
+package max.seed.me.speedplugin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class SpeedPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1), true);
+    }
+}
